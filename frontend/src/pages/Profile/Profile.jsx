3c8745a47b76426eb33363f4938f5403776af72b
Sanj33d <sanjeedsajeen8@gmail.com>
2025-11-14 19:16:25 +0600

    profile component half done

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext/AuthContext';
 
 const Profile = () => {
+    const {user} = useContext(AuthContext)
+
     return (
-        <div>
-            
+        <div className='text-center'>
+            {user ? <>
+            <p>Email: {user.email}</p>
+            <p>Name: {user.displayName}</p>
+            </> : <p>No user logged in</p>}
         </div>
     );
 };
--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -3,6 +3,7 @@ import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
 import Register from "../pages/Register/Register";
 import SignIn from "../pages/SignIn/SignIn";
+import Profile from "../pages/Profile/Profile";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
         {
             path: '/signIn',
             Component: SignIn
+        },
+        {
+          path: '/profile',
+          Component: Profile
         }
     ]
   },
