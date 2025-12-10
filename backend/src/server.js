5c804347075cb5588bf93f64eb3e7af4068ac886
Sanj33d <sanjeedsajeen8@gmail.com>
2025-12-10 11:30:21 +0600

    new branch feature/productCatalogue created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,43 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/database');
+const PORT = process.env.PORT || 5000;
+// const connectDB = require('./config/database');
 
 const app = express();
 
+// middleware
 app.use(cors());
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-connectDB();
 
-const productRoutes = require('./routes/productRoutes');
-app.use('/api/products', productRoutes);
+app.get('/', (req, res) => {
+    res.send('backend of smartTemu')
+})
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({
-        success: false,
-        message: 'Something went wrong!',
-        error: process.env.NODE_ENV === 'development' ? err.message: {}
-    });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+// module.exports = app;
+
+
+
+// app.use(express.urlencoded({ extended: true }));
+
+// connectDB();
+
+// const productRoutes = require('./routes/productRoutes');
+// app.use('/api/products', productRoutes);
+
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).json({
+//         success: false,
+//         message: 'Something went wrong!',
+//         error: process.env.NODE_ENV === 'development' ? err.message: {}
+//     });
+// });
 
 
 // Health check
@@ -30,16 +45,11 @@ app.use((err, req, res, next) => {
 //     res.json({ status: 'OK' });
 //   });
 
-app.use((req, res, next) => {
-    res.status(404).json({
-        success: false,
-        message: 'Route not found'
-    });
-});
+// app.use((req, res, next) => {
+//     res.status(404).json({
+//         success: false,
+//         message: 'Route not found'
+//     });
+// });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
-module.exports = app;
