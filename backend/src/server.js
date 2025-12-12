e2cc29b89304bc65ef68f9f9e60ee28a1660c2f7
Sanj33d <sanjeedsajeen8@gmail.com>
2025-12-12 14:06:06 +0600

    backnd + db connected, fetching done

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,6 +28,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // fetching from db
+    const productsCollection = client.db('test').collection('products')
+
+    app.get('/products', async (req, res) => {
+        const cursor = productsCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
